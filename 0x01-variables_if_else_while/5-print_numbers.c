bce364a866652939dcda1abb3265c0749f92d961
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-17 11:50:18 -0800

    CharmQuark

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int h;
+
+	for (h = 0; h < 10; h++)
+		printf("%i", h);
+
+	return (0);
+
+}
