92c8315334bfdc7f57c1685b2b2b4c4d01ed3ed2
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-17 03:21:02 -0800

    CharmQuark

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-  char phrase [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+  char phrase [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
   write(1,phrase,59);
   return (1);
 }
