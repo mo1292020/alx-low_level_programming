7523c6390752aae8ce0660429f15f6d89b7472f9
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-17 15:59:07 -0800

    CharmQuark

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int h;
+	int d;
+
+	for (h = 0; h < 9; h++)
+	{
+		for (d = h + 1; d < 10; d++)
+		{
+
+			putchar(h + 48);
+			putchar(d + 48);
+
+			if (h != 8 && d != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+}
