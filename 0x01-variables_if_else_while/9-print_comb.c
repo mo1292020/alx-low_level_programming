1f8309c4e88289ee701b63ee53541a4c13600e32
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-17 15:44:28 -0800

    CharmQuark

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int h;
+
+	for (h = 0; h < 10; h++)
+	{
+
+		putchar(h);
+
+		if (h != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+}
