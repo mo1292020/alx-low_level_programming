5708c7764489bdb7ef3a0c887244071cf7139fdc
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-17 17:00:07 -0800

    CharmQuark

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,24 @@
 int main(void)
 {
 	int h, d, m, n;
+	int a[10][10][10][10];
 
 	for (h = 0; h < 10; h++)
 	{
 		for (d = 0 ; d < 9; d++)
 		{
-			for (m = h; m < 10; m++)
+			for (m = 0; m < 10; m++)
 			{
 				for (n = 0; n < 10; n++)
 				{
 					if (h == m && d == n)
 						continue;
-					
+					if (a[h][d][m][n] == 1 || a[m][n][h][d] == 1)
+						continue;
+
+					a[h][d][m][n] = 1;
+					a[m][n][h][d] = 1;
+
 					putchar(h + '0');
 					putchar(d + '0');
 					putchar(' ');
