669ac5a0c801c3ef252152778df44389e7c1f883
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-21 09:19:54 -0800

    CharmQuark

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+
+	int h, d;
+
+	for (h = 0; h < 10; h++)
+	{
+		for (d = 97; d < 123; d++)
+			_putchar(d);
+	}
+
+	_putchar('\n');
+
+}
