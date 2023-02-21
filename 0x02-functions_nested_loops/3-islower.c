156a99907126807a31f78c87dbc3eee56b1418f7
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-21 14:31:55 -0800

    CharmQuark

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * int _islower - function to check char case
+ *
+ * Return: Always 1 if c is lowercase and 0 if uppercase
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (0);
+	else
+		return (1);
+}
