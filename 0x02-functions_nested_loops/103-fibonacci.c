f2b777b381a5f16363c25135a3d41ced0b9e28e2
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-23 06:50:36 -0800

    CharmQuark

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long int h, x = 1, y = 2, sum = 0;
+
+
+	while (y < 4000000)
+	{
+
+		if (y % 2 == 0)
+			sum += y;
+
+		y += x;
+		x = y - x;
+
+	}
+
+	printf("%lu\n", sum);
+
+
+	return (0);
+
+}
