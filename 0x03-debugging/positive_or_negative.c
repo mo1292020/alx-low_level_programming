9a257eddadab124716fafed9ec23b515cc2ce4e2
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-02-23 16:04:39 -0800

    CharmQuark

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
+	else
+		printf("%i is zero\n", i);
+
+}
