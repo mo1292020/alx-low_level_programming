10a9dca102d1c5d9798ba91a735c27591c5ad112
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-01 11:44:12 -0800

    CharmQuark

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,11 +8,55 @@
  * Return: Always 0 (Success)
 */
 
-int main(void)
+int  main(void)
 {
-	int n;
+
+	int arr[8], frr[8];
+	char s[8];
+	int n = 0, h;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	return (n);
+
+	for (h = 0; h < 8; h++)
+		frr[h] = 0;
+
+	for (h = 0; h < 8; h++)
+	{
+		while (frr[n] == 1)
+			n = (rand() % (8 - 0) + 0);
+
+		arr[h] = n;
+		frr[n] = 1;
+	}
+
+	for (h = 0; h < 8; h++)
+		frr[h] = 0;
+
+
+	n = 0;
+
+
+	for (h = 0; h < 5; h++)
+	{
+		while (frr[n] == 1)
+			n = (rand() % (10 - 0) + 0);
+
+		s[arr[h]] = n + 48;
+		frr[n] = 1;
+	}
+
+	n = (rand() % (122 - 97) + 97);
+
+	s[arr[5]] = n;
+
+	n = (rand() % (90 - 65) + 65);
+
+	s[arr[6]] = n;
+	n = (rand() % (47 - 33) + 33);
+	s[arr[7]] = n;
+
+	for (h = 0; h < 8; h++)
+		printf("%c", s[h]);
+
+	return (0);
 }
