b90d5d5343d811f092b8d1333b04eb8a246c6eef
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-08 07:59:43 -0800

    CharmQuark

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - that prints the sum of the two
+ * diagonals of a square matrix of integers.
+ *
+ * @a: input pointer
+ * @size: int parameter
+ *
+ * Return: nothing
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int h;
+	long int res1 = 0, res2 = 0;
+
+	for (h = 0; h < size * size; h++)
+	{
+		if (h % (size + 1) == 0)
+			res1 += a[h];
+		if (h % (size - 1) == 0 && h % (size + 1) != 0)
+			res2 += a[h];
+	}
+	printf("%lu, %lu", res1, res2);
+
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c~
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - Function that prints the chessboard.
+ *
+ * @a: input pointer
+ *
+ * Return: nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int h, d;
+
+	for (h = 0; h < 8; h++)
+	{
+		for (d = 0; d < 8 ; d++)
+			printf("%c", a[h][d]);
+
+		printf("\n");
+	}
+
+}
