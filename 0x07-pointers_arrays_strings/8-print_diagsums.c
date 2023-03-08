31971c81d3aebcecd9274153a1674164ee6d76d9
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-08 09:00:30 -0800

    CharmQuark

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	{
 		if (h % (size + 1) == 0)
 			res1 += a[h];
-		if (h % (size - 1) == 0 && h % (size + 1) != 0)
+		if (h % (size - 1) == 0 && h != 0 && h != size * size - 1)
 			res2 += a[h];
 	}
 	printf("%lu, %lu", res1, res2);
