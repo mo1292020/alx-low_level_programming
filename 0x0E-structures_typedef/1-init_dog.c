3db8dcc84516bb4e34c7cf9ac55402f0595eba7a
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-20 11:31:41 -0700

    CharmQuark

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -13,7 +14,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		(*d).name = name;
 		(*d).age = age;
