8e3cf3a7fc509481400ff4e157874515c0a50660
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-20 12:15:21 -0700

    CharmQuark

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog -  function that prints a struct dog.
+ *
+ * @d: dog parameter.
+ *
+ * Return: nothing.
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+		printf("Age : %f\n", d->age);
+		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	}
+}
