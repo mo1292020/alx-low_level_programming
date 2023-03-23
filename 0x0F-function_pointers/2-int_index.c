f0666ea1d4aacc16b624765034cde50a69e04da6
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-23 07:14:27 -0700

    CharmQuark

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,34 @@
-#include <stddef.h>
 #include "function_pointers.h"
 
 /**
- * int_index - a function that searches for an integer.
+ * int_index - a function that searches for an integer
  *
- * @array: array of integer parameter
- * @size: size_t parameter
- * @cmp: function pointer
+ * @array: pointer to array
+ * @size: size of @array
+ * @cmp: pointer to function call to check index in array
+ *       if it matches
  *
- * Return: the index of the first element for which the cmp
- * function does not return 0
- * If no element matches, return -1
- * If size <= 0, return -1
-**/
+ * Return: Always 0 (Success)
+*/
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (size <= 0)
-		return (-1);
-
-	int h;
+	int index;
+	bool y;
 
 	if (array != NULL && size > 0 && cmp != NULL)
-		for (h = 0; h < size; h++)
-			if (cmp(array[h]) != 0)
-				return (h);
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (index = 0; index < size; index++)
+		{
+			y = cmp(array[index]);
+			if (y == TRUE)
+				return (index);
+		}
+	}
+
 	return (-1);
+
 }
