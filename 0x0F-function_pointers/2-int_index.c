8315806a0c83b3a0e6c95a876bc6a8ed56ed5b2f
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-23 06:36:03 -0700

    CharmQuark

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+
+/**
+ * int_index - a function that searches for an integer.
+ *
+ * @array: array of integer parameter
+ * @size: size_t parameter
+ * @cmp: function pointer
+ *
+ * Return: the index of the first element for which the cmp
+ * function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+**/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	int h;
+
+	if (array != NULL && size > 0 && cmp != NULL)
+		for (h = 0; h < size; h++)
+			if (cmp(array[h]) != 0)
+				return (h);
+	return (-1);
+}
