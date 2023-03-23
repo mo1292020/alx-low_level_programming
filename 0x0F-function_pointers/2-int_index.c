934fd52dde69710e3289910a18a24c6f4c199284
mo1292020 <mohamed.ibrahem.lotfy.02468@gmail.com>
2023-03-23 07:18:40 -0700

    CharmQuark

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,6 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
-	bool y;
 
 	if (array != NULL && size > 0 && cmp != NULL)
 	{
@@ -23,8 +22,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 		for (index = 0; index < size; index++)
 		{
-			y = cmp(array[index]);
-			if (y == TRUE)
+			if (cmp(array[index]) != 0)
 				return (index);
 		}
 	}
